5d4085d168ed559c25f61aef3b813eb395714feb
Julian Lamprecht <51859473+Juli-EXP@users.noreply.github.com>
2021-04-12 17:35:03 +0200

    Fixed problem with .env file

--- backend/src/routes/bitbucket/test.js
+++ backend/src/routes/bitbucket/test.js
@@ -1,27 +1,31 @@
 /**
  * This is a class for testing
  */
-
 const router = require("express").Router();
 const {Bitbucket} = require("bitbucket");
 
+
 const clientOptions = {
     baseUrl: "https://api.bitbucket.org/2.0",
+    auth: {
+        username: process.env.BITBUCKET_USERNAME,
+        password: process.env.BITBUCKET_APP_PASSWORD,
+    },
 };
 
 const bitbucket = new Bitbucket(clientOptions);
 
 
 //Test to get information about a specific repository
-router.get("repository/:workspace/:repo_slug", async (req, res) => {
+router.get("/repo/:workspace/:repo_slug", async (req, res) => {
     try {
         const {data} = await bitbucket
             .repositories
             .get({workspace: req.params.workspace, repo_slug: req.params.repo_slug});
         res.send(data);
     } catch (err) {
-        const {error} = err;
-        console.log(error);
+        const {message, error} = err;
+        console.log("ERROR: ", error, message);
         res.sendStatus(404);
     }
 });
