935270e26433608d5525263536d469eaefb70e54
Daniel Nagler <danielnagler1507@gmail.com>
2021-04-14 11:56:31 +0200

    edit: sidebar fix 2

--- frontend/my-app/src/Components/Sidebar/Sidebar.js
+++ frontend/my-app/src/Components/Sidebar/Sidebar.js
@@ -1,32 +1,43 @@
 import React,{useState} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+
 import SidebarElement from "./Sidebarelement";
 
+import ServerContainer from "../AllServers/ServerContainer"
+//import Rerpository from "../TabRepositories/repositories"
+//import Scrum from "../TabScrum/scrum"
 
-
-import {FiServer} from 'react-icons/fi'
+import {HiServer} from 'react-icons/hi'
 import {IoLogoBitbucket} from 'react-icons/all'
 import {BiCalendarEvent} from 'react-icons/bi'
 
 const Sidebar = () => {
     const [url, setUrl] = useState()
+   
     return (
-        <div id="sidebarMainContainer" className="bg-primary fixed rounded-0.938 h-maxHeight w-14.625 pr-1.188 pl-1.188 pt-1.688 ">
-            <SidebarElement 
-                icon = {<FiServer color={`${url === "Server" ? "white" : "#94A3BC"}`} ></FiServer>}
-                title="Server"
-                url={url}
-                setUrl={setUrl}/>
-            <SidebarElement 
-                icon = {<IoLogoBitbucket color={`${url === "Repository" ? "white" : "#94A3BC"}`} ></IoLogoBitbucket>}
-                title="Repository"
-                url={url}
-                setUrl={setUrl}/>
-            <SidebarElement 
-                icon = {<BiCalendarEvent color={`${url === "Scrum" ? "white" : "#94A3BC"}`} ></BiCalendarEvent>}
-                title="Scrum"
-                url={url}
-                setUrl={setUrl}/>
-        </div>
+        <header id="sidebarMainContainer" className="bg-primary fixed rounded-0.938 h-maxHeight w-14.625 pr-1.188 pl-1.188 pt-1.688 ">
+            <Router>
+                <SidebarElement 
+                    icon = {HiServer}
+                    title="Server"
+                    url={url}
+                    setUrl={setUrl}/>
+                <SidebarElement
+                    icon = {IoLogoBitbucket}
+                    title="Repository"
+                    url={url}
+                    setUrl={setUrl}/>
+                <SidebarElement
+                    icon = {BiCalendarEvent}
+                    title="Scrum"
+                    url={url}
+                    setUrl={setUrl}/>
+            </Router>
+        </header>
     );
 };
 
