46fd23eced45529eb6d048a8978cc5fbd5882699
Beniamin Terza <beniaminterza@gmail.com>
2021-04-14 12:48:58 +0200

    fix: sidebar links route

--- frontend/my-app/src/Components/Sidebar/Sidebar.js
+++ frontend/my-app/src/Components/Sidebar/Sidebar.js
@@ -1,42 +1,39 @@
-import React,{useState} from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
-
+import React, { useState } from "react";
 import SidebarElement from "./Sidebarelement";
-
-import ServerContainer from "../AllServers/ServerContainer"
+import ServerContainer from "../AllServers/ServerContainer";
 //import Rerpository from "../TabRepositories/repositories"
 //import Scrum from "../TabScrum/scrum"
 
-import {HiServer} from 'react-icons/hi'
-import {IoLogoBitbucket} from 'react-icons/all'
-import {BiCalendarEvent} from 'react-icons/bi'
+import { HiServer } from "react-icons/hi";
+import { IoLogoBitbucket } from "react-icons/all";
+import { BiCalendarEvent } from "react-icons/bi";
 
 const Sidebar = () => {
-    const [url, setUrl] = useState()
-   
+    const [url, setUrl] = useState();
+
     return (
-        <header id="sidebarMainContainer" className="bg-primary fixed rounded-0.938 h-maxHeight w-14.625 pr-1.188 pl-1.188 pt-1.688 ">
-            <Router>
-                <SidebarElement 
-                    icon = {HiServer}
-                    title="Server"
-                    url={url}
-                    setUrl={setUrl}/>
-                <SidebarElement
-                    icon = {IoLogoBitbucket}
-                    title="Repository"
-                    url={url}
-                    setUrl={setUrl}/>
-                <SidebarElement
-                    icon = {BiCalendarEvent}
-                    title="Scrum"
-                    url={url}
-                    setUrl={setUrl}/>
-            </Router>
+        <header
+            id="sidebarMainContainer"
+            className="bg-primary fixed rounded-0.938 h-maxHeight w-14.625 pr-1.188 pl-1.188 pt-1.688 "
+        >
+            <SidebarElement
+                icon={HiServer}
+                title="Server"
+                url={url}
+                setUrl={setUrl}
+            />
+            <SidebarElement
+                icon={IoLogoBitbucket}
+                title="Repository"
+                url={url}
+                setUrl={setUrl}
+            />
+            <SidebarElement
+                icon={BiCalendarEvent}
+                title="Scrum"
+                url={url}
+                setUrl={setUrl}
+            />
         </header>
     );
 };
--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <div className="App h-full w-full p-8 bg-background">
+            <App />
+        </div>
     </React.StrictMode>,
     document.getElementById("root")
 );
