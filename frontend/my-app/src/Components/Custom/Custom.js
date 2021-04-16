74a408301f126ae94a467b7aac1ba8909d074ca3
Beniamin Terza <beniaminterza@gmail.com>
2021-04-16 16:41:46 +0200

    feat: custom chart

--- frontend/my-app/src/Components/Custom/Custom.js
+++ frontend/my-app/src/Components/Custom/Custom.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Chart from "./Chart";
 
 export default function Repository({ setUrl }) {
     const [name, setName] = useState("Repositoryname");
@@ -11,8 +12,9 @@ export default function Repository({ setUrl }) {
 
     return (
         <div className="ml-16.625 pb-8">
-            <div grid grid-flow-rows grid-cols-4 gap-8 mt-8>
-                //add dings
+            <div className="grid grid-flow-rows grid-cols-4 gap-8 mt-8">
+                <Chart />
+                <div>Hallo</div>
             </div>
         </div>
     );
