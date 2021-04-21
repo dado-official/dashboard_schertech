165a8f7553d1a51231d3e738decf4c9029445f64
Julian Lamprecht <julil.lamprecht@gmail.com>
2021-04-21 09:43:27 +0200

    Added custom to routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const repository = require("./bitbucket/repository");
 const server = require("./server");
+const custom = require("./custom");
 
 router.use("/repository", repository);
 router.use("/server", server);
+router.use("/custom", custom);
 
 module.exports = router;
