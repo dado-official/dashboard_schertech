9af84ce0647cce59ae23aa7da69f9fbe0aeb815e
Beniamin Terza <beniaminterza@gmail.com>
2021-04-23 18:10:55 +0200

    feat: repos add button

--- frontend/my-app/src/Components/AllRepositories/AddRepository.js
+++ frontend/my-app/src/Components/AllRepositories/AddRepository.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../Shared/Input";
 import TextArea from "../Shared/TextArea";
-import axios from 'axios';
+import axios from "axios";
 import { Link, useHistory } from "react-router-dom";
 
-
-const AddRepository = () => {
+const AddRepository = ({ setIsPopover, isPopover }) => {
     const [reponame, setReponame] = useState("");
     const [workspace, setWorkspace] = useState("");
     const [reposlug, setReposlug] = useState("");
@@ -13,44 +12,39 @@ const AddRepository = () => {
     const history = useHistory();
 
     function subm() {
-
         let data = {
             workspace: workspace,
             repo_slug: reposlug,
             name: reponame,
-            description: description
-        }
+            description: description,
+        };
 
-        axios.post("http://localhost:4000/api/repository", data).then((res) => { 
-            history.push("/repository")
+        axios.post("http://localhost:4000/api/repository", data).then((res) => {
+            history.push("/repository");
         });
     }
 
     return (
-        <div className="main h-full flex flex-wrap content-center">
-            <div className=" bg-primary rounded-0.938 w-22.625  m-auto p-1.875 pt-2.75 ">
-                <p className=" text-center text-white mb-2.813">
-                    Add Repository
-                </p>
-                <p className=" text-white text-xs ">Repository Name</p>
-                <Input state={reponame} setState={setReponame}></Input>
-                <p className=" text-white text-xs ">Workspace</p>
-                <Input state={workspace} setState={setWorkspace}></Input>
-                <p className=" text-white text-xs ">Repo slug</p>
-                <Input state={reposlug} setState={setReposlug}></Input>
-                <p className=" text-white text-xs ">Description</p>
-                <TextArea
-                    state={description}
-                    setState={setDescription}
-                ></TextArea>
-                <button
-                    
-                    onClick={subm}
-                    className="focus:outline-none rounded-0.625 w-full py-2 px-2 text-center text-white bg-commitBlue mb-2.75"
-                >
-                    Add Repository
-                </button>
-            </div>
+        <div
+            className={`bg-input rounded-0.938 w-26 p-12 z-10 absolute right-0 top-14 border-onlineGreen border-4 ${
+                isPopover ? "" : "hidden"
+            }`}
+        >
+            <p className="text-white mb-5 text-xl">Add Repository</p>
+            <p className=" text-white text-xs ">Repository Name</p>
+            <Input state={reponame} setState={setReponame}></Input>
+            <p className=" text-white text-xs ">Workspace</p>
+            <Input state={workspace} setState={setWorkspace}></Input>
+            <p className=" text-white text-xs ">Repo slug</p>
+            <Input state={reposlug} setState={setReposlug}></Input>
+            <p className=" text-white text-xs ">Description</p>
+            <TextArea state={description} setState={setDescription}></TextArea>
+            <button
+                onClick={subm}
+                className="focus:outline-none rounded-0.625 w-full py-2 px-2 text-center text-white bg-commitBlue mb-2.75"
+            >
+                Add Repository
+            </button>
         </div>
     );
 };
--- frontend/my-app/src/Components/AllRepositories/AllRepositories..js
+++ frontend/my-app/src/Components/AllRepositories/AllRepositories..js
@@ -1,10 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import RepositoriesContaier from "./RepositoriesContainer";
-import AddButton from "../Shared/AddButton";
-import axios from 'axios'
+import axios from "axios";
+import { FaAngleDown } from "react-icons/fa";
+import AddRepository from "./AddRepository";
 
-export default function AllServers({ setUrl, props}) {
+export default function AllServers({ setUrl, props }) {
     const [data, setData] = useState([]);
+    const [isPopover, setIsPopover] = useState(false);
+
+    const addServerRef = useRef();
+
+    useEffect(() => {
+        let handler = (e) => {
+            if (
+                addServerRef.current &&
+                !addServerRef.current.contains(e.target)
+            ) {
+                setIsPopover(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handler);
+
+        return () => {
+            document.removeEventListener("mousedown", handler);
+        };
+    });
 
     useEffect(() => {
         setUrl("Repository");
@@ -15,21 +36,45 @@ export default function AllServers({ setUrl, props}) {
 
     return (
         <div className="main">
-            <h2 className={`text-white text-2xl font-medium`}>
-                All Repositories
-            </h2>
-            <p className=" text-unclicked">All the current repositories</p>
-            <div className="grid grid-flow-row responsiveGrid gap-8 mt-4">
-                {data != [] ? data.map((element) => (
-                    <RepositoriesContaier
-                        name={element.name}
-                        description={element.description}
-                        workspace={element.workspace}
-                        repo_slug={element.repo_slug}
-                        id={element.id}
+            <div className="relative flex justify-between items-baseline">
+                <div>
+                    <h2 className={`text-white text-2xl font-medium`}>
+                        All Repositories
+                    </h2>
+                    <p className=" text-unclicked">
+                        All the current repositories
+                    </p>
+                </div>
+                <div ref={addServerRef}>
+                    <button
+                        onClick={() => {
+                            setIsPopover((prev) => !prev);
+                        }}
+                        className="py-2 px-6 bg-onlineGreen focus:outline-none outline-none rounded-0.625 font-medium text-black"
+                    >
+                        <div className=" flex items-center gap-2">
+                            Add Server
+                            <FaAngleDown color="black" size="18" />
+                        </div>
+                    </button>
+                    <AddRepository
+                        setIsPopover={setIsPopover}
+                        isPopover={isPopover}
                     />
-                )):<br/>}
-                <AddButton title="repository" />
+                </div>
+            </div>
+            <div className="grid grid-flow-row responsiveGrid gap-8 mt-4">
+                {data != []
+                    ? data.map((element) => (
+                          <RepositoriesContaier
+                              name={element.name}
+                              description={element.description}
+                              workspace={element.workspace}
+                              repo_slug={element.repo_slug}
+                              id={element.id}
+                          />
+                      ))
+                    : null}
             </div>
         </div>
     );
